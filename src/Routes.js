9fa7e6f3995b4835ce024187ab0596f6e6b133fe
maulana <maulana20@versatech.co.id>
2019-04-28 11:24:00 +0700

    demo show images
    Signed-off-by: maulana <maulana20@versatech.co.id>

--- src/Routes.js
+++ src/Routes.js
@@ -3,14 +3,18 @@ import {Router, Stack, Scene} from 'react-native-router-flux';
 
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Hello from './demo/Hello';
+import Bananas from './demo/Bananas';
 
 export default class Routes extends Component<{}> {
 	render() {
 		return(
 			<Router>
 				<Stack key="root" hideNavBar={true}>
-					<Scene key="login" component={Login} title="Login" initial={true}/>
+					<Scene key="login" component={Login} title="Login"/>
 					<Scene key="signup" component={Signup} title="Register"/>
+					<Scene key="hello" component={Hello} title="Hello"/>
+					<Scene key="bananas" component={Bananas} title="Bananas" initial={true}/>
 				</Stack>
 			</Router>
 		)
